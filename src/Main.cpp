e4beaf66b78b1aca1816c33573b11f7905691715
Joshua Koo <zz85nus@gmail.com>
2015-02-14 15:51:40 +0800

    Use embindings for cpp to js communication
    - uses HLSLParser.onSuccess and HLSLParser.onError in JS scope
    - Exposes: parseHLSL

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,25 +5,37 @@
 #include "HLSLTree.h"
 
 /*
-TODO
-- type
-- filename
-- file contents
-- entry name
-
-parseHLSL2GLSL
+Exposes: parseHLSL
+uses HLSLParser.onSuccess and HLSLParser.onError in JS scope
+For more info on bindings, see
+http://kripken.github.io/emscripten-site/docs/porting/connecting_cpp_and_javascript/Interacting-with-code.html#interacting-with-code-call-function-pointers-from-c
 */
+
+#include <emscripten/val.h>
+
+#define str(str) std::string(str)
 
 extern "C" {
 
-    int parse(char* source, char* fileName, char* entryName)
+    int parseHLSL(char* source, char* entryName, char* type)
     {
         using namespace M4;
 
-        Log_Error(source);
-        Log_Error("..fileName...\n");
+        const char* fileName = "filename.hlsl";
 
+        emscripten::val HLSLParserBridge = emscripten::val::global("HLSLParser");
+
+        // emscripten::val console = emscripten::val::global("console");
+        // console.call<void>("log", str("Source: "), str(source));
+        // console.call<void>("log", str("Filename: "), str(fileName));
+        // console.call<void>("log", str("type: "), str(type));
+        // console.call<void>("log", str("Entry: "), str(entryName));
+
+        // fragment shader by default
         GLSLGenerator::Target target = GLSLGenerator::Target_FragmentShader;
+        if (String_Equal(type, "vs")) {
+            target = GLSLGenerator::Target_VertexShader;
+        }
 
         // Parse input file
         Allocator allocator;
@@ -31,20 +43,24 @@ extern "C" {
         HLSLTree tree(&allocator);
         if (!parser.Parse(&tree))
         {
-            Log_Error("Parsing failed, aborting");
+            // Log_Error("Parsing failed, aborting");
+            HLSLParserBridge.call<void>("onError", str("parsing failed"));
             return 1;
         }
 
         // Generate output
         GLSLGenerator generator(&allocator);
         if (!generator.Generate(&tree, target, entryName)) {
-            Log_Error("Generation error.\n");
+            // Log_Error("Generation error.\n");
+            HLSLParserBridge.call<void>("onError", str("code generation failed"));
             return EXIT_FAILURE;
         }
-        Log_Error(generator.GetResult());
+
+        const char *result = generator.GetResult();
+        HLSLParserBridge.call<void>("onSuccess", str(result));
+
+        // Log_Error(result);
 
         return 0;
     }
-
 }
-
